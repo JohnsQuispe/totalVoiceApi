113b80fee049448c3f0926564bd3e879bb057e15
gustavoaquino <guuaquino@hotmail.com>
2019-12-11 00:28:53 -0300

    UNVERSION FILES

---
+++ src/main/java/br/com/johnsquispe/totalVoiceApi/TotalVoiceApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.johnsquispe.totalVoiceApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotalVoiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotalVoiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/johnsquispe/totalVoiceApi/TotalVoiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.johnsquispe.totalVoiceApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotalVoiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
