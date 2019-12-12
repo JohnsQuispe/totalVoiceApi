4f287f0fa3be19fafcdc0178d542934cdb7f8d90
Eduardo <eduardojguedes@hotmail.com>
2019-12-11 22:26:42 -0300

    Criado Model e Repository

---
+++ src/main/java/br/com/johnsquispe/totalVoiceApi/models/Usuario.java
@@ -0,0 +1,68 @@
+package br.com.johnsquispe.totalVoiceApi.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long usuarioId;
+    private String login;
+    private String senha;
+    private boolean logado;
+    private Date dataUltimoAcesso;
+    private Date dataCadastro;
+
+    public Long getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Long usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public boolean isLogado() {
+        return logado;
+    }
+
+    public void setLogado(boolean logado) {
+        this.logado = logado;
+    }
+
+    public Date getDataUltimoAcesso() {
+        return dataUltimoAcesso;
+    }
+
+    public void setDataUltimoAcesso(Date dataUltimoAcesso) {
+        this.dataUltimoAcesso = dataUltimoAcesso;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+}
